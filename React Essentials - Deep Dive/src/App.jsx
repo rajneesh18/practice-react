9d738e8bf93c7e39dfb3363870d3ae9952f302aa
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2024-12-10 11:22:18 +0530

    immutability matters - always

--- React Essentials - Deep Dive/src/App.jsx
+++ React Essentials - Deep Dive/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for(const turn of gameTurns) {
     const {square, player} = turn;
@@ -67,6 +67,10 @@ function App() {
     });
   }
 
+  function handlerRestart() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -74,7 +78,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handlerRestart} />}
         <GameBoard 
           onSelectSquare={handleSelectSquare} 
           board={gameBoard}
